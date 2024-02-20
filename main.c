023707e41040f051cb574e440cc1f65dade236da
Marco Ceccarelli <maceccar@c2r1p12.42firenze.it>
2024-02-20 16:06:59 +0100

    prova

--- main.c
+++ main.c
@@ -5,8 +5,13 @@ int	main(int argc, char **argv)
 	t_stack	*stack;
 
 	stack = load_stack(argc, argv, stack);
-	//ft_display_stack(stack);
-	stack = sort_stack(stack);
+
+	ft_display_stack(stack);
+	ft_printf("\n");
+	ft_pop_tail(stack);
+	ft_display_stack(stack);
+	
+	//stack = sort_stack(stack);
 
 	ft_free_stack(stack);
 }
